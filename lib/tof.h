4ad137125cc53f04a053035eeb5dbb7de802434d
Crossbow2560 <nishit.bilimagga@gmail.com>
2025-08-28 15:27:02 +0530

    Add header file for ReRoTOF class

---
+++ lib/tof.h
@@ -0,0 +1,21 @@
+#ifndef TOF_H
+#define TOF_H
+#include <Arduino.h>
+#include <Wire.h>
+#include <DFRobot_VL6180X.h>
+
+class ReRoTOF {
+public:
+    ReRoTOF(const int* cePins, const uint8_t* addrs, size_t count);
+    void begin();
+    uint8_t readDistance(uint8_t idx);
+
+private:
+    int* _cePins;
+    uint8_t* _addrs;
+    size_t _count;
+    DFRobot_VL6180X* _tof;
+    void init_TOF(uint8_t i);
+};
+
+#endif
