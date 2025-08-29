d9a8b5ddda39942f59deae452981c33f4d95f584
Crossbow2560 <nishit.bilimagga@gmail.com>
2025-08-29 11:29:30 +0530

    Add IR class and methods for pin input handling

---
+++ lib/ir.cpp
@@ -0,0 +1,20 @@
+#include "ir.h"
+
+IR :: IR(int s, int* pinArray){
+  size = s;
+  for(int i = 0; i < size; i++){
+    pinMode(pinArray[i], INPUT);
+  }
+}
+
+int getAll(){
+  int* valArray[size];
+  for(int i = 0; i < N; i++){
+    valArray[i] = digitalRead(pinArray[i]);
+  }
+  return valArray;
+}
+
+int IR :: getVal(int index){
+  return digitalRead(pinArray[index]);
+}
