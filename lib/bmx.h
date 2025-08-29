835699866b47884668c70cacfb501a567b5dd3b6
Crossbow2560 <nishit.bilimagga@gmail.com>
2025-08-29 15:26:05 +0530

    Add BMX class header for sensor data handling

---
+++ lib/bmx.h
@@ -0,0 +1,19 @@
+#ifndef BMX_H
+#define BMX_H
+
+#include <Arduino.h>
+#include <DFRobot_BMX160.h>
+
+class BMX{
+  private:
+    DFRobot_BMX160 sensor;
+    sBmx160SensorData_t Ogyro, Oaccel;
+  public:
+    BMX();
+    void init();
+    sBmx160SensorData* getGyroData();
+    sBmx160SensorData* getAccelData();
+
+}
+
+#endif
