8e07959ac17165b586549ab4e2bd90893a437396
Crossbow2560 <nishit.bilimagga@gmail.com>
2025-08-29 11:25:15 +0530

    Update Class

--- lib/ir.h
+++ lib/ir.h
@@ -1,22 +1,13 @@
-#ifndef PINARRAY_H
-#define PINARRAY_H
+#ifndef IR_H
+#define IR_H
 
-#include <Arduino.h>
-
-class PinArray {
-public:
-    PinArray(const int* pins, int size);
-    void begin();
-    void readAll();
-    void printAll(Stream& stream = Serial);
-
-    int getValue(int index) const;
-    int getSize() const;
-
-private:
-    const int* pinArr;
-    int* valArr;
-    int n;
+class IR{
+    private:
+        int size;
+        int* pinArray;
+    public:
+        IR(int size, int* pinArray);
+        int* getAll();
+        int getValue(int index);
+        
 };
-
-#endif // PINARRAY_H
